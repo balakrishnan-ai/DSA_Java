da086fd4345cf8ba8498324a2e5bc5d31853c7f7
balakrishnan-ai <198842863+balakrishnan-ai@users.noreply.github.com>
2025-08-30 11:31:29 +0530

    DSA Class-1
    ArrayList: [Bala, Mani, kannan]
    LinkedList: [10, 20, 30]
    Stack (pop) : C++
    Queue (poll): Task1
    HashMap: {101=Balakrishnan, 102=Manikandan, 103=Kannan}
    HashSet: [Chennai, Madurai]

---
+++ DataStructureEg.java
@@ -0,0 +1,51 @@
+package lava1;
+
+import java.util.*;
+
+public class DataStructureEg {
+
+	public static void main(String[] args) {
+		List<String> names=new ArrayList<>();
+		names.add("Bala");
+		names.add("Mani");
+		names.add("kannan");
+		System.out.println("ArrayList: "+names);
+		
+		//LinkedList Example
+		LinkedList<Integer> numbers=new LinkedList<>();
+		numbers.add(10);
+		numbers.add(20);
+		numbers.add(30);
+		System.out.println("LinkedList: "+numbers);
+		
+		//Stack Example
+		Stack<String> stack=new Stack<>();
+		stack.push("Java");
+		stack.push("Python");
+		stack.push("C++");
+		System.out.println("Stack (pop) : "+stack.pop());
+		
+		//Queue Example
+		Queue <String> queue=new LinkedList<>();
+		queue.add("Task1");
+		queue.add("Task2");
+		System.out.println("Queue (poll): "+queue.poll());
+		
+		//HashMap Example
+		Map <Integer, String> stu=new HashMap<>();
+		stu.put(101, "Balakrishnan");
+		stu.put(102, "Manikandan");
+		stu.put(103,"Kannan");
+		System.out.println("HashMap: "+stu);
+		
+		//HashSet Example
+		Set<String> cities=new HashSet<>();
+		cities.add("Chennai");
+		cities.add("Madurai");
+		cities.add("Chennai");
+		System.out.println("HashSet: "+cities);
+		
+
+	}
+
+}
